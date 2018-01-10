2d8e429e259ec4a869c61a2cd56cd13e11b258b9
Petr Matukhov <petr.matukhov@gmail.com>
2018-01-10 20:09:35 +0300

    tarantool params

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
 # plainrest
-Тестовый сервер тестового скоринга
+Тестовый сервер тестового скоринга.
+
+В качестве хранилища используется Tarantool.
 
 ### Запуск сервера
 Пример: `python api.py --log=server.log --port=8080`
 
 * `port` - порт, по-умолчанию 8080
 * `log` - путь к логам сервера (если не указан, лог пишется в stdout)
+* `tarantool_host` - хост Tarantool (Тарантул работает хранилищем)
+* `tarantool_port` - порт Tarantool
+* `tarantool_login` - логин для Tarantool
+* `tarantool_passwotd` - пароль для Tarantool
+
 
 ### Запуск тестов
 
 `python test.py`
+
+Для тестов с хранилищем нужно, чтобы в системе был установлен Docker. 
+
+Перед запуском тестов необходимо выполнить `docker-compose up -d` 
--- api.py
+++ api.py
@@ -410,14 +410,29 @@ if __name__ == "__main__":
     op = OptionParser()
     op.add_option("-p", "--port", action="store", type=int, default=8080)
     op.add_option("-l", "--log", action="store", default=None)
+    op.add_option("--tarantool_host", action="store", default='localhost')
+    op.add_option("--tarantool_port", action="store", default=3301)
+    op.add_option("--tarantool_login", action="store", default='score_user')
+    op.add_option("--tarantool_password", action="store", default='score_pass')
     (opts, args) = op.parse_args()
     logging.basicConfig(filename=opts.log, level=logging.INFO,
                         format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
 
     try:
-        store = store.StoreTarantool()
+        store = store.StoreTarantool(
+            host=opts.tarantool_host, 
+            port=opts.tarantool_port, 
+            user=opts.tarantool_login, 
+            password=opts.tarantool_password
+            )
+
+        logging.info('Storage %s:%s init sucess',  
+            opts.tarantool_host, opts.tarantool_port)
+
     except Exception as e:
-        logging.info('Store init failed, starting with no store. Error: %s', e.message)
+        logging.info('Storage %s:%s init failed, starting with no store. ' 
+            'Error: %s', 
+            opts.tarantool_host, opts.tarantool_port, e.message)
         store = None
 
     server = HTTPServer(("localhost", opts.port), MainHTTPHandler)
